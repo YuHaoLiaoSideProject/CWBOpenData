1ebefa15c2a4fbb93d36ecc12e6dacbe76759ae6
yuhaoliao <h770320@gmail.com>
2020-08-26 17:40:08 +0800

    加入 BaseRepository

---
+++ CWBOpenData/CWBOpenData/Attributes/AutoKeyAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace CWBOpenData.Attributes
+{
+    /// <summary>
+    /// 自動流水號 PrimaryKey
+    /// </summary>
+    public class AutoKeyAttribute : PrimaryKeyAttribute
+    {
+
+    }
+}
---
+++ CWBOpenData/CWBOpenData/Attributes/PrimaryKeyAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace CWBOpenData.Attributes
+{
+    /// <summary>
+    /// 用來標記EF model的 PK
+    /// </summary>
+    public class PrimaryKeyAttribute : System.Attribute
+    {
+
+    }
+}
